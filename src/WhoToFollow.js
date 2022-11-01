cd57e0cb3e572bfc8784b7ce46c65ad4fadbd48b
Asad Zaidi <asadzaidi@asads-air.home>
2022-11-01 07:41:47 +0000

    Widgets

--- src/WhoToFollow.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const WhoToFollow = () => {
-  return <div>WhoToFollow</div>
-}
-
-export default WhoToFollow
--- src/Widgets.js
+++ src/Widgets.js
@@ -1,11 +1,38 @@
 import React from "react"
-import WhoToFollow from "./WhoToFollow"
+import NewsTile from "./NewsTile"
+import WhoToFollowTile from "./WhoToFollowTile"
 import "./widgets.css"
 
 const Widgets = () => {
   return (
     <div className="widgets">
-      <WhoToFollow />
+      <div className="widget">
+        <h2 className="widget-title">What's happening</h2>
+        <div className="widget-tile">
+          <NewsTile></NewsTile>
+        </div>
+        <div className="widget-tile">
+          <NewsTile></NewsTile>
+        </div>
+        <div className="widget-tile">
+          <NewsTile></NewsTile>
+        </div>
+        <div className="widget-tile widget-show-more">Show more</div>
+      </div>
+
+      <div className="widget">
+        <h2 className="widget-title">Who to follow</h2>
+        <div className="widget-tile">
+          <WhoToFollowTile></WhoToFollowTile>
+        </div>
+        <div className="widget-tile">
+          <WhoToFollowTile></WhoToFollowTile>
+        </div>
+        <div className="widget-tile">
+          <WhoToFollowTile></WhoToFollowTile>
+        </div>
+        <div className="widget-tile widget-show-more">Show more</div>
+      </div>
     </div>
   )
   // TODO Who to follow widget
