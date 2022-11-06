8d2f62318c76ebfeaf1ff12dc5ede603941e88da
Asad Zaidi <asadzaidi@asads-air.home>
2022-11-06 11:36:53 +0000

    Removed and renamed classes from post

--- src/Post.js
+++ src/Post.js
@@ -8,15 +8,11 @@ import "./post.css"
 const Post = ({ name, handle, date, tweet, image }) => {
   return (
     <div className="post">
-      <div className="post-avatar-wrapper">
-        <Avatar src="https://cdn.pixabay.com/photo/2016/03/28/12/35/cat-1285634_1280.png" />
-      </div>
-      <div className="post-content">
+      <Avatar src="https://cdn.pixabay.com/photo/2016/03/28/12/35/cat-1285634_1280.png" />
+      <div className="post-body">
         <PostHeader name={name} handle={handle} date={date}></PostHeader>
-        <div className="post-content-tweet">{tweet}</div>
-        {image && <img className="post-content-image" src={image}></img>}
-        {/* TODO Too much space because of padding around icon */}
-        {/* TODO Make the footer more squashed to the left */}
+        <div>{tweet}</div>
+        {image && <img className="post-image" src={image}></img>}
         <PostFooter></PostFooter>
       </div>
     </div>
